b9a9b331c1b64e506445d3e7d4602703f7713982
basarat <basaratali@gmail.com>
2022-02-06 10:00:54 +1100

    back to start

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 function update(button: number, person: Person | null) {
-  if (button == 1 && person) {
-    if (person.age != null) {
-      if (person.experience != null) {
-        person.experience += 10;
-        person.age++;
+  if (button == 1) {
+    if (person) {
+      if (person.age != null) {
+        if (person.experience != null) {
+          person.experience += 10;
+          person.age++;
+        } else {
+          throw new Error('Person experience not set');
+        }
       } else {
-        throw new Error('Person experience not set');
+        throw new Error('Person age not set');
       }
-    } else {
-      throw new Error('Person age not set');
     }
   }
 }
