8fddd512b214a25900227abe920cfba720ea0de9
basarat <basaratali@gmail.com>
2022-02-06 19:40:58 +1100

    wrap

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 function update(button: number, person: Person | null) {
-  if (button === 1) {
-    if (person) {
-      if (person.age != null) {
-        if (person.experience != null) {
-          person.experience += 10;
-          person.age++;
-        } else {
-          throw new Error('Person experience not set');
-        }
-      } else {
-        throw new Error('Person age not set');
-      }
-    }
-  }
+  if (button !== 1 || !person) { return; }
+  if (person.age == null) { throw new Error('Person age not set'); }
+  if (person.experience == null) { throw new Error('Person experience not set'); }
+
+  person.experience += 10;
+  person.age++;
 }
